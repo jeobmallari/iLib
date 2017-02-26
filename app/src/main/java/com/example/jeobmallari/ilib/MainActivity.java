cd908c1498bccea66592702be54737b30a0c1c14
Jeob Mallari <jeobmallari@gmail.com>
2017-02-26 23:50:28 +0800

    first commit
    dami pang gagawin hahaha.

---
+++ app/src/main/java/com/example/jeobmallari/ilib/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.jeobmallari.ilib;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] listEntries;
+    private DrawerLayout dl;
+    private ListView drawerList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listEntries = getResources().getStringArray(R.array.list_array);
+        dl = (DrawerLayout) findViewById(R.id.activity_main);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+    }
+}
