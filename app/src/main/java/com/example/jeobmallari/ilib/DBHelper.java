0ed420d597e39ff36cb794cead1ee7a121c01e14
Jeob Mallari <jeobmallari@gmail.com>
2017-02-28 10:37:40 +0800

    blah
    something better

---
+++ app/src/main/java/com/example/jeobmallari/ilib/DBHelper.java
@@ -0,0 +1,8 @@
+package com.example.jeobmallari.ilib;
+
+/**
+ * Created by Jeob Mallari on 2/28/2017.
+ */
+
+public class DBHelper {
+}
--- app/src/main/java/com/example/jeobmallari/ilib/MainActivity.java
+++ app/src/main/java/com/example/jeobmallari/ilib/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
     private String[] listEntries;
     private DrawerLayout dl;
     private ListView drawerList;
-
+    private Button loginButton;
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         listEntries = getResources().getStringArray(R.array.list_array);
         dl = (DrawerLayout) findViewById(R.id.activity_main);
         drawerList = (ListView) findViewById(R.id.left_drawer);
+
+    }
+
+    protected void checkLogin(){
+
+    }
+
+    protected void setSignup(){
 
     }
 }
